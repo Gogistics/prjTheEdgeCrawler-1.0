8d90ab9f96fdbbc9dea8bc1b90c68b6e9ac94da7
Alan Tai <gogistics.tw@gmail.com>
2015-04-18 10:05:14 -0700

    update crawler

--- crawler_1_0.js
+++ crawler_1_0.js
@@ -1,1 +1,16 @@
+# required modules
 GLOBAL.crawler = require('crawler');
+GLOBAL.mongojs = require('mongojs');
+
+# init config.
+GLOBAL.config_crawler = GLOABL.config_crawler || {};
+GLOBAL.config_mongojs = GLOBAL.config_mongojs || {};
+
+# config mongojs
+config_mongojs.db_url = '';
+config_mongojs.db_collections = [''];
+
+GLOBAL.my_mongo = mongojs.connect(config_mongojs.db_url, config_mongojs.db_collections);
+
+# set new crawler
+GLOBAL.crawler_1 = new crawler();
