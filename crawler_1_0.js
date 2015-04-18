554a8a2e48e715d7ed32ff41c354148dbcb83abe
Alan Tai <gogistics.tw@gmail.com>
2015-04-18 10:41:50 -0700

    update crawler

--- crawler_1_0.js
+++ crawler_1_0.js
@@ -1,16 +1,25 @@
 # required modules
 GLOBAL.crawler = require('crawler');
 GLOBAL.mongojs = require('mongojs');
+GLOBAL.cheerio = require('cheerio');
 
 # init config.
 GLOBAL.config_crawler = GLOABL.config_crawler || {};
 GLOBAL.config_mongojs = GLOBAL.config_mongojs || {};
 
 # config mongojs
-config_mongojs.db_url = '';
-config_mongojs.db_collections = [''];
+config_mongojs.db_url = 'localhost/the_edge';
+config_mongojs.db_collections = ['funding_lending'];
 
 GLOBAL.my_mongo = mongojs.connect(config_mongojs.db_url, config_mongojs.db_collections);
 
 # set new crawler
-GLOBAL.crawler_1 = new crawler();
+config_crawler.setting = {maxConnection : 10,
+			forceUTF8 : true};
+config_crawler.setting.callback = function(err, result){
+	if(!err && result.statusCode == 200){
+
+	}
+};
+
+GLOBAL.crawler_1 = new crawler(config_crawler.setting);
