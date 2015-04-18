a27fa05c3c1311345ede6ff95c1fd0e3313cc110
Alan Tai <gogistics.tw@gmail.com>
2015-04-18 12:07:34 -0700

    update crawler

--- crawler_1_0.js
+++ crawler_1_0.js
@@ -14,13 +14,17 @@ config_mongojs.db_collections = ['funding_lending'];
 GLOBAL.my_mongo = mongojs.connect(config_mongojs.db_url, config_mongojs.db_collections);
 
 # set new crawler
+config_crawler.url = 'http://www.borro.com';
 config_crawler.setting = {maxConnection : 10,
 			forceUTF8 : true};
 config_crawler.setting.callback = function(err, result){
 	if(!err && result.statusCode == 200){
-
+		var $ = cheerio.load(result.load);
+		$('a').each(function(index, value){
+			
+		});
 	}
 };
 
 GLOBAL.crawler_1 = new crawler(config_crawler.setting);
-GLOBAL.crawler_1.queue('http://www.borro.com');
+GLOBAL.crawler_1.queue(config_crawler.url);
