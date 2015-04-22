ced7f9c47a56aaa37581efeb51474dd1359e54da
Alan Tai <gogistics.tw@gmail.com>
2015-04-22 13:12:20 -0700

    update crawler

--- crawler_1_0.js
+++ crawler_1_0.js
@@ -1,30 +1,53 @@
-# required modules
+/* required modules */
 GLOBAL.crawler = require('crawler');
 GLOBAL.mongojs = require('mongojs');
 GLOBAL.cheerio = require('cheerio');
 
-# init config.
-GLOBAL.config_crawler = GLOABL.config_crawler || {};
+/* init config. */
+GLOBAL.config_crawler = GLOBAL.config_crawler || {};
 GLOBAL.config_mongojs = GLOBAL.config_mongojs || {};
 
-# config mongojs
-config_mongojs.db_url = 'localhost/the_edge';
-config_mongojs.db_collections = ['funding_lending'];
+// config mongojs
+// config_mongojs.db_url = 'localhost/the_edge';
+// config_mongojs.db_collections = ['funding_lending'];
+// GLOBAL.my_mongo = mongojs.connect(config_mongojs.db_url, config_mongojs.db_collections);
 
-GLOBAL.my_mongo = mongojs.connect(config_mongojs.db_url, config_mongojs.db_collections);
+/* set new crawler */
+GLOBAL.config_crawler.url = 'http://www.borro.com';
+GLOBAL.config_crawler.setting = {maxConnection : 10,
+			forceUTF8 : true,
+			callback : function(err, result){
+					if(!err && result.statusCode == 200){
+						var $ = cheerio.load(result.body);
+						$('a').each(function(index, value){
+							var sub_url = $(this).attr('href');
+							if(sub_url !== undefined){
+								var result = sub_url.match(/javascript|pdf|mailto|tel/gi);
+								if(!result){
+									if(sub_url.indexOf('\/') === 0){
+										sub_url = sub_url.substring(1);
+										
+									}
+									if(sub_url.indexOf('http') !== 0){
+										sub_url = GLOBAL.config_crawler.url + sub_url;
+									}
 
-# set new crawler
-config_crawler.url = 'http://www.borro.com';
-config_crawler.setting = {maxConnection : 10,
-			forceUTF8 : true};
-config_crawler.setting.callback = function(err, result){
-	if(!err && result.statusCode == 200){
-		var $ = cheerio.load(result.load);
-		$('a').each(function(index, value){
-			
-		});
-	}
-};
+									var str = $(this).text() || $(this).attr('title');
+									if(str !== undefined){
+										str = str.replace(/\t|\r|\n/gi, '').trim();
+									}else{
+										str = 'NA';
+									}
 
-GLOBAL.crawler_1 = new crawler(config_crawler.setting);
-GLOBAL.crawler_1.queue(config_crawler.url);
+									// print
+									console.log('Text: ' + str + ' ; URL: ' + sub_url);
+								}
+							}
+							// console.log(value);			
+						});
+					}
+				}
+			};
+
+GLOBAL.crawler_1 = new crawler(GLOBAL.config_crawler.setting);
+GLOBAL.crawler_1.queue(GLOBAL.config_crawler.url);
