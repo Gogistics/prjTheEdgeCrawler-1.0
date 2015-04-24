cc9bb156aaa6b91f42fc8b8d251d02077b5331f2
AlanTai <rainman.tai@gmail.com>
2015-04-23 18:45:12 -0700

    update

--- crawler_1_0.js
+++ crawler_1_0.js
@@ -13,7 +13,7 @@ config_mongojs.db_collections = ['crowd_funding_lending'];
 GLOBAL.my_mongo = mongojs.connect(config_mongojs.db_url, config_mongojs.db_collections);
 
 /* set new crawler */
-GLOBAL.config_crawler.url = 'https://fund364.com/';
+GLOBAL.config_crawler.url = 'https://www.lendup.com/';
 GLOBAL.config_crawler.setting = {maxConnection : 10,
 								 forceUTF8 : true,
 								};
