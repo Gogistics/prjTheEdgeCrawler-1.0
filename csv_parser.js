abf9faf34bfad0ffc1fe06325da648ff9806845c
AlanTai <rainman.tai@gmail.com>
2015-04-29 19:10:48 -0700

    update .csv parser

--- csv_parser.js
+++ csv_parser.js
@@ -4,21 +4,28 @@ var fs = require('fs'),
 
 /* file paths */
 var csvReadStream = fs.createReadStream("/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/lending_club/LoanStats3a.csv");
- 
-/* var */
-var count = 0, keys;
 
+var csvWriteStream = csv.createWriteStream({ headers : true }),
+    csvWritableStream = fs.createWriteStream("/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/lending_club/parsedLoanStats3a.csv");
+csvWritableStream.on("finish", function(){
+	console.log("done with writable stream.");
+});
+csvWriteStream.pipe(csvWritableStream);
+
+/* readable stream */
+var count = 0, keys;
 var csvReadableStream = csv()
     .on("data", function(data){
 		if(count === 0){
 			keys = data;
+			csvWriteStream.write(keys);
 		}
 		count += 1;
          // console.log(data);
     })
     .on("end", function(){
          console.log("done");
-		 console.log(keys);
+		 csvWriteStream.end();
     });
  
 csvReadStream.pipe(csvReadableStream);
