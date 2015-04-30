b7a4052be176b92d9a4d258ab966b48f17e27edd
AlanTai <rainman.tai@gmail.com>
2015-04-29 18:42:22 -0700

    update .csv parser

--- csv_parser.js
+++ csv_parser.js
@@ -1,14 +1,23 @@
+/* required modules */
 var fs = require('fs'), 
     csv = require('fast-csv');
 
+/* file paths */
 var stream = fs.createReadStream("/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/lending_club/LoanStats3b.csv");
  
+/* var */
+var count = 0, keys;
+
 var csvStream = csv()
     .on("data", function(data){
-         console.log(data);
+		if(count === 0){
+			keys = data;
+		}
+         // console.log(data);
     })
     .on("end", function(){
          console.log("done");
+		 console.log(data);
     });
  
 stream.pipe(csvStream);
