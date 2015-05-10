f425b3379fa2154acfc7aa1ffbf725bcd4ca5c40
AlanTai <rainman.tai@gmail.com>
2015-05-10 12:37:54 -0700

    update parser and add new converter

--- csv_to_json_converter.js
+++ csv_to_json_converter.js
@@ -49,7 +49,7 @@ GLOBAL.build_data_obj = function(arg_key, arg_file_path){
 								var keyword_index = csv_keys.indexOf("keyword"), keyword_number_index = csv_keys.indexOf("number");
 								var keyword = data[keyword_index], keyword_number = data[keyword_number_index];
 								
-								GLOBAL.data_obj["children"]["children"]push( { name : keyword, size : keyword_number } );
+								GLOBAL.data_obj["children"]["children"].push( { name : keyword, size : keyword_number } );
 							}
 							count += 1;
 						})
