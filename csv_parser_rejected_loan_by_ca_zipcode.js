95bd8abbc3b746abdf3495368b816178c8284a81
AlanTai <rainman.tai@gmail.com>
2015-06-03 11:45:39 -0700

    update classifier

--- csv_parser_rejected_loan_by_ca_zipcode.js
+++ csv_parser_rejected_loan_by_ca_zipcode.js
@@ -122,8 +122,7 @@ GLOBAL.async_parser.parse_files = function (arg_files){
 																
 																
 																GLOBAL.async_parser.manipulated_obj[GLOBAL.async_parser.current_zipcode]['loan_types'] = {};
-																GLOBAL.async_parser.manipulated_obj[GLOBAL.async_parser.current_zipcode]['loan_types']['loan_type'] = loan_type;
-																GLOBAL.async_parser.manipulated_obj[GLOBAL.async_parser.current_zipcode]['loan_types']['count'] = 1;
+																GLOBAL.async_parser.manipulated_obj[GLOBAL.async_parser.current_zipcode]['loan_types'][loan_type] = 1;
 																
 														}else if(GLOBAL.async_parser.current_state !== undefined &&
 																GLOBAL.async_parser.current_state === "CA" &&
@@ -147,7 +146,7 @@ GLOBAL.async_parser.parse_files = function (arg_files){
 																	
 																	// update loan types
 																	if(GLOBAL.async_parser.manipulated_obj[GLOBAL.async_parser.current_zipcode]['loan_types'].hasOwnProperty(loan_type)){
-																		GLOBAL.async_parser.manipulated_obj[GLOBAL.async_parser.current_zipcode]['loan_types']['loan_type'] += 1;
+																		GLOBAL.async_parser.manipulated_obj[GLOBAL.async_parser.current_zipcode]['loan_types'] += 1;
 																	}else{
 																		GLOBAL.async_parser.manipulated_obj[GLOBAL.async_parser.current_zipcode]['loan_types']['loan_type'] = 1;
 																	}
