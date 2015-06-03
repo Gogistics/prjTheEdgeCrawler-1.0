875b3507dad5cb08708a526afbc755c0c5539e62
AlanTai <rainman.tai@gmail.com>
2015-06-03 08:49:09 -0700

    update training set and classifier

--- csv_natural_language_create_classifier.js
+++ csv_natural_language_create_classifier.js
@@ -1,8 +1,25 @@
 /* required modules */
 var natural = require('natural'),
-	jsonfile = require('jsonfile');
+	jsonfile = require('jsonfile'),
+	natural = require('natural');
 
+GLOBAL.classifier = new natural.BayesClassifier();
+GLOBAL.classifier.events.on('trainedWithDocument', function (obj) {
+   console.log(obj);
+});
 GLOBAL.training_set_path = '/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/lending_club/parsedRejectResultTrainingSet.json';
-jsonfile.readFile(training_set_path, function(err, obj){
-	console.log(obj);
+jsonfile.readFile(training_set_path, function(err, training_set){
+	training_set.forEach(function(elem, index){
+		GLOBAL.classifier.addDocument(elem.sentence, elem.keyword); 
+	});
+	
+	// train classifier
+	GLOBAL.classifier.train();
+	
+	// save classifier
+	GLOBAL.classifier.save('/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/lending_club/rejectLoansClassifier.json', function(err, classifier){
+		if (!err){
+			console.log('done');
+		}
+	});
 })
