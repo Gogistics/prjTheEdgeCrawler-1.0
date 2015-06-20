01778e5e0510673424786a9230f715154c85abbc
AlanTai <rainman.tai@gmail.com>
2015-06-20 09:55:57 -0700

    add csv line reader

---
+++ csv_line_reader.js
@@ -0,0 +1,16 @@
+var fs = require('fs');
+
+function get_line(filename, line_no, callback) {
+    var data = fs.readFileSync(filename, 'utf8');
+    var lines = data.split("\n");
+
+    if(+line_no > lines.length){
+      throw new Error('File end reached without finding line');
+    }
+
+    callback(null, lines[+line_no]);
+}
+
+get_line('/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/lending_club/LoanStats3a.csv', 2, function(err, line){
+  	console.log('The line: ' + line);
+})
