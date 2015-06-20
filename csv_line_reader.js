d6cba67fe8ef163f28cc7c7633dc3d33519d3b42
AlanTai <rainman.tai@gmail.com>
2015-06-20 10:21:21 -0700

    add csv line reader

--- csv_line_reader.js
+++ csv_line_reader.js
@@ -13,6 +13,8 @@ function get_line(filename, line_no, callback) {
 
 get_line('/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/lending_club/LoanStats3a.csv', 0, function(err, line){
   	var data = line.replace(/"/g, '');
+	console.log(data);
 	data = line.split(',');
+	console.log(data);
 	console.log(data[data.indexOf('url')]);
 })
