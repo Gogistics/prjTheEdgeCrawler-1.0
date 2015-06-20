85853c9a3313b56e179b044b6bfc5d4d1a0562d7
AlanTai <rainman.tai@gmail.com>
2015-06-20 12:43:42 -0700

    add csv line reader

--- csv_line_reader.js
+++ csv_line_reader.js
@@ -7,14 +7,24 @@ function get_line(filename, line_no, callback) {
     if(+line_no > lines.length){
       throw new Error('File end reached without finding line');
     }
-
     callback(null, lines[+line_no]);
+	
+  	var data = lines[0].replace(/"/g, '');
+	data = data.split(',');
+	var index = data.indexOf('url');
+	//
+	lines.forEach(function(elem, index){
+		if(index !== 0){
+			console.log(elem.splite(',')[index]);
+		}
+	});
 }
 
 get_line('/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/lending_club/LoanStats3a.csv', 0, function(err, line){
-  	var data = line.replace(/"/g, '');
+  	/* var data = line.replace(/"/g, '');
 	console.log(data);
 	data = data.split(',');
 	console.log(data);
-	console.log(data[data.indexOf('url')]);
+	console.log(data[data.indexOf('url')]); */
+	console.log('done...');
 })
