2e16a6fd1de5ae0e086d50853bdaf9eafa270054
AlanTai <rainman.tai@gmail.com>
2015-06-20 13:18:48 -0700

    add csv line reader

--- csv_line_reader.js
+++ csv_line_reader.js
@@ -12,7 +12,7 @@ function get_line(filename, line_no, callback) {
 	//
 	lines.forEach(function(elem, ith){
 		if(ith !== 0){
-			var url_regex = /https.+?\d+/g;
+			var url_regex = /(https.+\?\d+)/;
 			url = url_regex.exec(elem);
 			console.log(url);
 		}
