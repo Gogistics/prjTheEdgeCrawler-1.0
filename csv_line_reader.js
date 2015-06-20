e7d552f8d465c36318b44373e321486453bcbd7f
AlanTai <rainman.tai@gmail.com>
2015-06-20 13:26:52 -0700

    add csv line reader

--- csv_line_reader.js
+++ csv_line_reader.js
@@ -12,9 +12,9 @@ function get_line(filename, line_no, callback) {
 	//
 	lines.forEach(function(elem, ith){
 		if(ith !== 0){
-			var url_regex = /^https.+\?loan_id\=\d+$/;
-			url = url_regex.exec(elem);
-			console.log(elem);
+			var url_regex = /^https.+\?loan_id\=\d+$/,
+				url = url_regex.exec(elem);
+			console.log(url);
 		}
 	});
 }
