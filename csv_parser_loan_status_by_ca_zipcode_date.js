39adf6c098ccc28feefb0d7e6f05b28a64dec703
AlanTai <rainman.tai@gmail.com>
2015-06-26 16:21:25 -0700

    update parser

--- csv_parser_loan_status_by_ca_zipcode_date.js
+++ csv_parser_loan_status_by_ca_zipcode_date.js
@@ -66,7 +66,6 @@ GLOBAL.async_parser.parse_files = function (arg_files){
 															try{
 																var file_path = './lendingclub/loan_stats_' + GLOBAL.async_parser.id + '.txt' ;
 																body = fs.readFileSync(file_path).toString();
-																console.log('working fine...');
 															}catch( err ){
 																console.log('sth wrong...');
 																var url = 'https://www.lendingclub.com/browse/loanDetail.action?loan_id=' + GLOBAL.async_parser.id;
@@ -74,7 +73,6 @@ GLOBAL.async_parser.parse_files = function (arg_files){
 																body = res.getBody();
 																if ( body === undefined || body === null ) return false;
 															}finally{
-																console.log(GLOBAL.async_parser.id);
 																if ( body === undefined || body === null ) return false;
 																var $ = cheerio.load(body);
 																$('table.loan-details').each(function(){
@@ -104,7 +102,9 @@ GLOBAL.async_parser.parse_files = function (arg_files){
 																GLOBAL.async_parser.date = current_year + '-' + current_month + '-' + current_day;
 																GLOBAL.async_parser.current_date_of_loan = GLOBAL.async_parser.date;
 																if( ( Number(month_day_year_ary[2]) + 2000 ) < 2007 || ( Number(month_day_year_ary[2]) + 2000 ) > 2014){
+																	console.log(GLOBAL.async_parser.id);
 																	console.log(GLOBAL.async_parser.date);
+																	return false;
 																}
 															}
 														}else{
