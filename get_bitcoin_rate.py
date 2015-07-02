7a594e2597803f9f857f08250c03c5490d992ef0
AlanTai <rainman.tai@gmail.com>
2015-07-01 17:31:56 -0700

    update parser to analyze the latest data

--- get_bitcoin_rate.py
+++ get_bitcoin_rate.py
@@ -6,30 +6,30 @@ class BitcoinHandler():
         self._coindesk_url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
         self._coinbase_url = 'https://api.coinbase.com/v1/currencies/exchange_rates'
         self._bitstamp_url = 'https://www.bitstamp.net/api/ticker/'
-        self._btc_e_url = 'https://www.bitstamp.net/api/ticker/'
+        self._btc_e_url = 'https://btc-e.com/api/2/btc_usd/ticker'
         
     def get_coinbase_exchange_rate(self):
         resp = urllib2.urlopen(self._coinbase_url)
         content = json.loads( resp.read() )
-        print content
+        print content['btc_to_usd']
         return content
         
     def get_coindesk_exchange_rate(self):
         resp = urllib2.urlopen(self._coindesk_url)
         content = json.loads( resp.read() )
-        print content
+        print content['bpi']['USD']['rate']
         return content
         
     def get_bitstamp_exchange_rate(self):
         resp = urllib2.urlopen(self._bitstamp_url)
         content = json.loads( resp.read() )
-        print content
+        print content['last']
         return content
         
     def get_btc_e_exchange_rate(self):
         resp = urllib2.urlopen(self._btc_e_url)
         content = json.loads( resp.read() )
-        print content
+        print content['ticker']['avg']
         return content
         
 if __name__ == "__main__":
