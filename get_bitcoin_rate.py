0fefbd7092e08952231376634a6bb7d1e475ced2
AlanTai <rainman.tai@gmail.com>
2015-07-01 17:53:45 -0700

    update parser to analyze the latest data

--- get_bitcoin_rate.py
+++ get_bitcoin_rate.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import urllib2, json
+import urllib2, json, time
 
 class BitcoinHandler():
     def __init__(self):
@@ -11,26 +11,55 @@ class BitcoinHandler():
     def get_coinbase_exchange_rate(self):
         resp = urllib2.urlopen(self._coinbase_url)
         content = json.loads( resp.read() )
-        print content['btc_to_usd']
+        rate = '{0:.2f}'.format(content['btc_to_usd'])
+
+        today = time.strftime('%Y-%m-%d')
+        file_name = "btc_e-exchange_rate_{current_date}.txt".format(current_date = today)
+        file_path = "{path}{file_name}".format(path = './', file_name = file_name)
+        self.save_data(file_path, json.dumps(content))
         return content
         
     def get_coindesk_exchange_rate(self):
         resp = urllib2.urlopen(self._coindesk_url)
         content = json.loads( resp.read() )
-        print content['bpi']['USD']['rate']
+        rate = '{0:.2f}'.format(content['bpi']['USD']['rate'])
+
+        today = time.strftime('%Y-%m-%d')
+        file_name = "btc_e-exchange_rate_{current_date}.txt".format(current_date = today)
+        file_path = "{path}{file_name}".format(path = './', file_name = file_name)
+        self.save_data(file_path, json.dumps(content))
         return content
         
     def get_bitstamp_exchange_rate(self):
         resp = urllib2.urlopen(self._bitstamp_url)
         content = json.loads( resp.read() )
-        print content['last']
+        rate = '{0:.2f}'.format(content['last'])
+
+        today = time.strftime('%Y-%m-%d')
+        file_name = "btc_e-exchange_rate_{current_date}.txt".format(current_date = today)
+        file_path = "{path}{file_name}".format(path = './', file_name = file_name)
+        self.save_data(file_path, json.dumps(content))
         return content
         
     def get_btc_e_exchange_rate(self):
         resp = urllib2.urlopen(self._btc_e_url)
         content = json.loads( resp.read() )
-        print content['ticker']['avg']
+        rate = '{0:.2f}'.format(content['ticker']['avg'])
+
+        today = time.strftime('%Y-%m-%d')
+        file_name = "btc_e-exchange_rate_{current_date}.txt".format(current_date = today)
+        file_path = "{path}{file_name}".format(path = './', file_name = file_name)
+        self.save_data(file_path, json.dumps(content))
         return content
+        
+    def save_data(self, arg_file_path, arg_data):
+        try:
+            with open(arg_file_path, 'a') as f:
+                f.write(arg_data)
+                return true
+        except: BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
         
 if __name__ == "__main__":
     bitcoin_handler = BitcoinHandler()
