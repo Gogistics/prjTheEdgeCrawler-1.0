9f94ba5db3a62b6f8180dbb232667a3c89164349
AlanTai <rainman.tai@gmail.com>
2015-07-07 12:30:39 -0700

    add new crawler

---
+++ get_stock_price_p2p_lending.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import urllib2, json, time, csv
+
+class StockPriceHandler():
+    def __init__(self):
+        self._quandl_urls = {
+            'lendingclub' : 'http://download.finance.yahoo.com/d/quotes.csv?s=LC&f=sl1d1t1c1ohgv&e=.csv',
+            'lendingtree' : 'http://download.finance.yahoo.com/d/quotes.csv?s=TREE&f=sl1d1t1c1ohgv&e=.csv',
+            'tslx' : 'http://download.finance.yahoo.com/d/quotes.csv?s=TSLX&f=sl1d1t1c1ohgv&e=.csv'
+        }
+        
+        
+    def get_csv(self):
+        for key, val in self._quandl_urls.iteritems():
+            resp = urllib2.urlopen(val)
+            content = csv.reader( resp.read() )
+            
+            today = time.strftime('%Y-%m-%d')
+            file_name = "{platform}_{current_date}.csv".format(platform = key ,current_date = today)
+            file_dir = '/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/stock_market/lending/{sub_dir}/'.format( sub_dir = key)
+            file_path = "{path}{file_name}".format(path = , file_name = file_name)
+            self.save_csv(file_path, json.dumps(content))
+        
+    def save_csv(self, arg_file_path, arg_data):
+        try:
+            with open(arg_file_path, 'a') as f:
+                f.write(arg_data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+        
+if __name__ == "__main__":
+    bitcoin_handler = StockPriceHandler()
+    bitcoin_handler.get_csv()
