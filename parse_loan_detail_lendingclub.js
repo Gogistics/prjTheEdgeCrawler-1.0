48d903d9d418274e4bff46fe5711e6223106ca1b
AlanTai <rainman.tai@gmail.com>
2015-07-10 14:33:15 -0700

    update

--- parse_loan_detail_lendingclub.js
+++ parse_loan_detail_lendingclub.js
@@ -7,7 +7,7 @@ var fs = require('fs'),
 	
 /* */
 var parse_html_to_json = function(arg_file_path){
-	fs.readfile(arg_file_path, 'utf-8', function(err, file_html){
+	fs.readFile(arg_file_path, 'utf-8', function(err, file_html){
 		var $ = cheerio.load(file_html);
 		$('tr').each(function(index, value){
 			var grade = $('td').find('.rateAndAmountRequestd').find('span').text();
