68fa660b93cf483d5c54ab9c12577d905f0ae0c4
AlanTai <rainman.tai@gmail.com>
2015-07-10 14:32:10 -0700

    update

---
+++ parse_loan_detail_lendingclub.js
@@ -0,0 +1,32 @@
+/* required modules */
+var fs = require('fs'),
+	async = require('async'),
+	jsonfile = require('jsonfile'),
+	natural = require('natural'),
+	cheerio = require("cheerio");
+	
+/* */
+var parse_html_to_json = function(arg_file_path){
+	fs.readfile(arg_file_path, 'utf-8', function(err, file_html){
+		var $ = cheerio.load(file_html);
+		$('tr').each(function(index, value){
+			var grade = $('td').find('.rateAndAmountRequestd').find('span').text();
+			var grade_number = $('td').find('.rateAndAmountRequestd').find('span').find('span').text();
+			console.log(grade);
+			console.log(grade_number);
+		});
+	});
+}
+var parser_callback = function(err, files){
+	files.forEach(function(file, index){
+		parse_html_to_json(file);
+	});
+}
+
+var file_dir = '/var/www/prjTheEdge-Beta-1.0/media/static/frontend/files/lending_club/media';
+var get_and_parse_data = function(){
+	fs.readdir(file_dir, parser_callback);
+}
+
+/**/
+get_and_parse_data();
