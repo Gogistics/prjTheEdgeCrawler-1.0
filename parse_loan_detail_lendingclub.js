c38445cbce16734b5867330e53f3a08acccfebba
AlanTai <rainman.tai@gmail.com>
2015-07-10 17:00:29 -0700

    update

--- parse_loan_detail_lendingclub.js
+++ parse_loan_detail_lendingclub.js
@@ -15,6 +15,7 @@ var parse_html_to_json = function(arg_file_name){
 		if(file_html !== undefined){
 			var $ = cheerio.load(file_html);
 			$('tr').each(function(){
+				
 				var grade = $(this).find('td.rateAndAmountRequested')
 								.find('div')
 								.find('span.master_pngfix')
@@ -26,12 +27,23 @@ var parse_html_to_json = function(arg_file_name){
 								.trim(),
 					grade_number = $(this).find('td.rateAndAmountRequested').find('div').find('span').find('span').text().trim(),
 					rate = $(this).find('td.rateAndAmountRequested').find('div').find('strong').text().trim(),
-					loan_type_length = $(this).find('td.yui-dt1-col-typeAndTerm').find('div').find('span').text().trim();
+					loan_type_length = $(this).find('td.yui-dt1-col-typeAndTerm').find('div').find('span').text().trim(),
+					fico = $(this).find('td.yui-dt1-col-fico').find('div.ficoDisplay').text().trim(),
+					total_amount = $(this).find('td.yui-dt1-col-totalAmount').find('div.timeDisplay').text().trim(),
+					purpose = $(this).find('td.yui-dt1-col-titleAndPurpose').find('span.loan_purpose').text().trim(),
+					id = $(this).find('td.yui-dt1-col-titleAndPurpose').find('a.expand-loan-details').attr('href');
+					
+				var number_pattern = /\d+/g;	
+				id = id.match(number_pattern)[0];
 				
+				data_json['id'] = id;
 				data_json['grade'] = grade;
 				data_json['grade_number'] = grade_number;
 				data_json['rate'] = rate;
 				data_json['loan_type_length'] = loan_type_length;
+				data_json['fico'] = fico;
+				data_json['total_amount'] = total_amount;
+				data_json['purpose'] = purpose;
 					
 				console.log(data_json);
 			});
