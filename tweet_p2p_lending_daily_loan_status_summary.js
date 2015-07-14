600f38c91285a89ea93b6be9f28309258f2bf41b
AlanTai <rainman.tai@gmail.com>
2015-07-14 13:01:17 -0700

    update

--- tweet_p2p_lending_daily_loan_status_summary.js
+++ tweet_p2p_lending_daily_loan_status_summary.js
@@ -72,9 +72,10 @@ var loop_through_files_and_tweet = function(){
 	datetime_pdt = new Date(datetime_pdt);
 	datetime_pdt = datetime_pdt.toLocaleDateString() + ' ' + datetime_pdt.toLocaleTimeString() + ' PDT';
 	var summary_1 = 'daily loan status-' + datetime_pdt + ' Total Loans: ' + summary.total_loans + ' Avg. Requested Amt:$' + (summary.total_amt / summary.total_loans).toFixed(0) + ' @LendingClub #p2p_lending http://www.moneysedge.com/data_analysis?data_provider=lending_club&data_category=daily_loan_status';
-	var summary_1 = 'daily loan status-' + datetime_pdt + ' Total Loans: ' + summary.total_loans + ' Avg. Requested Amt:$' + (summary.total_rate / summary.total_loans).toFixed(2) + ' @LendingClub #p2p_lending http://www.moneysedge.com/data_analysis?data_provider=lending_club&data_category=daily_loan_status';
+	var summary_2 = 'daily loan status-' + datetime_pdt + ' Total Loans: ' + summary.total_loans + ' Avg. Requested Amt:' + (summary.total_rate / summary.total_loans).toFixed(2) + '% @LendingClub #p2p_lending http://www.moneysedge.com/data_analysis?data_provider=lending_club&data_category=daily_loan_status';
 	
 	console.log(summary_1);
+	console.log(summary_2);
 	// tweet_bitcoin_exchange_rate_summary(summary_1);
 }
 
