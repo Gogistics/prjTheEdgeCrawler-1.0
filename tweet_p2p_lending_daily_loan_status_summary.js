6175a9a10a219e5ef369eea90d569e628339c30a
AlanTai <rainman.tai@gmail.com>
2015-07-16 16:28:50 -0700

    update parser

--- tweet_p2p_lending_daily_loan_status_summary.js
+++ tweet_p2p_lending_daily_loan_status_summary.js
@@ -29,7 +29,7 @@ var get_stock_price = function( arg_file_path ){
     var lines = data.split("\n");
 	var latest_info = lines[lines.length - 2].split(',');
 	
-	return {'stock_price' : latest_info[1], 'percentage' : latest_info[4], 'date_edt' : latest_info[2], 'time_edt' : latest_info[3]};
+	return {'stock_price' : latest_info[1], 'percentage' : latest_info[4], 'date_edt' : latest_info[2].replace(/\"/g, ''), 'time_edt' : latest_info[3].replace(/\"/g, '') };
 }
 
 var tweet_p2p_lending_daily_summary = function( arg_status ){
@@ -92,7 +92,7 @@ var loop_through_files_and_tweet = function(){
 	var date = newest_stock_price_file.substring(newest_stock_price_file.indexOf('_') + 1,newest_stock_price_file.indexOf('.'));
 	
 	var summary_stock_price = get_stock_price(dir_lendingclub_stock_price + newest_stock_price_file);
-	console.log('LendingClub stock price: ' + summary_stock_price['date_edt'] + '(' + summary_stock_price['percentage'] + ') ' + summary_stock_price['time_edt'] + ' EDT @LendingClub #p2p_lending' );
+	console.log('LendingClub stock price: ' + summary_stock_price['stock_price'] + ' ' + summary_stock_price['date_edt'] + '(' + summary_stock_price['percentage'] + ') ' + summary_stock_price['time_edt'] + ' EDT @LendingClub #p2p_lending' );
 }
 
 /* tweet summary */
