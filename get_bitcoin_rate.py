13e4a41dff80d846043686bcc987b43cec8f6f3c
AlanTai <rainman.tai@gmail.com>
2015-07-20 18:26:07 -0700

    update

--- get_bitcoin_rate.py
+++ get_bitcoin_rate.py
@@ -5,8 +5,8 @@ class BitcoinHandler():
     def __init__(self):
         self._coindesk_url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
         self._coinbase_url = 'https://api.coinbase.com/v1/currencies/exchange_rates'
-        self._bitstamp_url = 'https://www.bitstamp.net/api/ticker/' # issue
-        self._btc_e_url = 'https://btc-e.com/api/2/btc_usd/ticker'
+        self._bitstamp_url = 'https://www.bitstamp.net/api/ticker/'
+        self._btc_e_url = 'https://btc-e.com/api/2/btc_usd/ticker' # issue
         self._itbit_url = 'https://api.itbit.com/v1/markets/XBTUSD/ticker'
         self._lakebtc_url = 'https://www.lakebtc.com/api_v1/ticker'
         self._okcoin_url = 'https://www.okcoin.com/api/v1/ticker.do?symbol=btc_usd'
@@ -177,6 +177,6 @@ if __name__ == "__main__":
     bitcoin_handler.get_okcoin_exchange_rate()
     bitcoin_handler.get_bitfinex_exchange_rate()
     bitcoin_handler.get_btc_q_exchange_rate()
-    bitcoin_handler.get_btc_e_exchange_rate()
+    # bitcoin_handler.get_btc_e_exchange_rate()
     # bitcoin_handler.get_bityes_exchange_rate()
     
